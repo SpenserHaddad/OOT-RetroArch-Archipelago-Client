1a471f8a9638922c3a89a789e426296014ce8425
Joshua Eagles <joshuaeagles1@gmail.com>
2024-07-30 18:51:25 -0400

    cleanup location check service and fix sometimes not seeing temp context checks

--- LocationCheckService.cs
+++ LocationCheckService.cs
@@ -9,46 +9,56 @@ public class LocationCheckService
 		_retroarchMemoryService = retroarchMemoryService;
 	}
 
-	// We're gonna want some sort of caching system for this so that it doesn't keep reporting every single location every single time
-	// Will worry about that after this works though
+	// Going to want some sort of caching system for this so that it doesn't keep reporting every single location every single time
+	// Might be as simple as a hashmap somewhere that gets loaded with all received items, and stuff only gets sent to the server when it's not in that hashmap
 	public async Task<List<string>> GetAllCheckedLocationNames()
 	{
-		var coopContext = await _retroarchMemoryService.ReadByteArray(address: 0xA040002c, numberOfBytes: 4);
+		var outgoingItemKey = await _retroarchMemoryService.ReadByteArray(address: 0x8040002c, numberOfBytes: 4);
+
+		// Since this is async with the emulator, there's a chance that the key
+		// gets populated after we read it but before we write to clear it
+		if (outgoingItemKey.Any((b) => b != 0x00))
+		{
+			await _retroarchMemoryService.WriteByteArray(
+				address: 0x8040002c,
+				dataToWrite: [00, 00, 00, 00]
+			);
+			await _retroarchMemoryService.WriteByteArray(
+				address: 0x80400030,
+				dataToWrite: [00, 00, 00, 00]
+			);
+		}
 
 		var checkedLocationNames = new List<string>();
 		foreach (var locationInformation in AllLocationInformation.LocationInformations)
 		{
-			if (await CheckLocation(locationInformation: locationInformation, coopContext: coopContext))
+			if (await CheckLocation(locationInformation: locationInformation, outgoingItemKey: outgoingItemKey))
 			{
 				checkedLocationNames.Add(locationInformation.Name);
 			}
 		}
 
-		await _retroarchMemoryService.WriteByteArray(
-			address: 0xA040002c,
-			dataToWrite: [00, 00, 00, 00, 00, 00, 00, 00]
-		);
-
 		return checkedLocationNames;
 	}
 
-	private async Task<bool> CheckLocation(LocationInformation locationInformation, byte[] coopContext)
+	private async Task<bool> CheckLocation(LocationInformation locationInformation, byte[] outgoingItemKey)
 	{
-		switch (locationInformation.Type)
+		return locationInformation.Type switch
 		{
-			case LocationType.Chest:
-				return await CheckChestLocation(locationInformation: locationInformation, coopContext: coopContext);
-			default:
-				throw new InvalidOperationException(
-					$"Unknown LocationType {locationInformation.Type} for location {locationInformation.Name}"
-				);
-		}
+			LocationType.Chest => await CheckChestLocation(
+				locationInformation: locationInformation,
+				outgoingItemKey: outgoingItemKey
+			),
+			_ => throw new InvalidOperationException(
+				$"Unknown LocationType {locationInformation.Type} for location {locationInformation.Name}"
+			)
+		};
 	}
 
-	private async Task<bool> CheckChestLocation(LocationInformation locationInformation, byte[] coopContext)
+	private async Task<bool> CheckChestLocation(LocationInformation locationInformation, byte[] outgoingItemKey)
 	{
-		return CoopContextCheck(
-				coopContext: coopContext,
+		return OutgoingKeyCheck(
+				outgoingItemKey: outgoingItemKey,
 				sceneOffset: locationInformation.Offset,
 				bitToCheck: locationInformation.BitToCheck,
 				ootrLocationType: 0x1
@@ -62,7 +72,7 @@ public class LocationCheckService
 
 	private async Task<bool> SceneCheck(byte sceneOffset, byte bitToCheck, byte sceneDataOffset)
 	{
-		const uint sceneFlagsOffset = 0xA011A6A4;
+		const uint sceneFlagsOffset = 0x8011A6A4;
 
 		var localSceneOffset = (uint)(sceneFlagsOffset + 0x1c * sceneOffset + sceneDataOffset);
 
@@ -71,8 +81,9 @@ public class LocationCheckService
 		return ((nearbyMemory >> bitToCheck) & 1) == 1;
 	}
 
-	private bool CoopContextCheck(byte[] coopContext, byte sceneOffset, byte bitToCheck, byte ootrLocationType)
+	private bool OutgoingKeyCheck(byte[] outgoingItemKey, byte sceneOffset, byte bitToCheck, byte ootrLocationType)
 	{
-		return coopContext[0] == sceneOffset && coopContext[1] == ootrLocationType && coopContext[3] == bitToCheck;
+		return outgoingItemKey[0] == sceneOffset && outgoingItemKey[1] == ootrLocationType &&
+			outgoingItemKey[3] == bitToCheck;
 	}
 }
