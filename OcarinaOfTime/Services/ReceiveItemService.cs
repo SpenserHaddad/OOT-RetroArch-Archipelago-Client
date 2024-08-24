91e823a12565b11bd0cf010556c17e2c6f4a0ed2
Joshua Eagles <joshuaeagles1@gmail.com>
2024-08-24 17:49:07 -0400

    add a guarded write to receiving items to avoid double receiving

--- OcarinaOfTime/Services/ReceiveItemService.cs
+++ OcarinaOfTime/Services/ReceiveItemService.cs
@@ -1,4 +1,5 @@
 using Archipelago.MultiClient.Net.Models;
+using OOT_AP_Client.Models;
 using OOT_AP_Client.Services.Interfaces;
 
 namespace OOT_AP_Client.OcarinaOfTime.Services;
@@ -31,6 +32,27 @@ public class ReceiveItemService
 
 		const uint incomingPlayerAddress = 0xA0400026;
 		const uint incomingItemAddress = 0xA0400028;
+
+		// Verify that the last item was processed already before trying to send another
+		var memoryReadCommands = new MemoryReadCommand[]
+		{
+			new()
+			{
+				Address = incomingPlayerAddress,
+				NumberOfBytes = 2,
+			},
+			new()
+			{
+				Address = incomingItemAddress,
+				NumberOfBytes = 2,
+			},
+		};
+		var currentIncomingMemoryState = await _memoryService.ReadMemoryToLongMulti(memoryReadCommands);
+		if (currentIncomingMemoryState[incomingPlayerAddress] != 0 ||
+			currentIncomingMemoryState[incomingItemAddress] != 0)
+		{
+			return;
+		}
 
 		await _memoryService.Write16(address: incomingPlayerAddress, dataToWrite: 0x00);
 		await _memoryService.Write16(address: incomingItemAddress, dataToWrite: (ushort)(item.ItemId - 66000));
